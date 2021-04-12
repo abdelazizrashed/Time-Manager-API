70a4c3e96437a4d6e1e5b3d592d1fc5c39f5bb19
abdelazizrashed <abdelaziz.y.rashed@gmail.com>
2021-04-12 16:41:54 +0200

    Finish implementing the resources of colors submodule

--- app/shared/color/resources.py
+++ app/shared/color/resources.py
@@ -1,27 +1,100 @@
 from flask import Flask
 from flask_restful import Resource, request
 from flask_jwt_extended import jwt_required, get_jwt
+from ..db_man.service import db
+from .models import ColorModel
+from .interfaces import ColorInterface
+from .services import ColorService
 
 class ColorResource(Resource):
 
     def __init__(self, app: Flask):
         self.app = app
 
-    @jwt_required()
     def get(self):
-        raise NotImplementedError
+        color_id: int = request.get_json()['color_id']
+        if not color_id:
+            return {
+                'description': "The color id must be given",
+                'error': "missing_info"
+            }, 400
+        color: ColorModel = ColorService.retrieve_by_color_id(color_id, self.app)
+        if not color:
+            return {
+                'description': "Color not found",
+                'error': "not_found"
+            }, 404
+        return ColorService.json(color), 200
 
     @jwt_required()
     def post(self):
-        raise NotImplementedError
+        claims = get_jwt()
+        if not claims['is_admin']:
+            return{
+                'description': "You need admin privileges",
+                'error': "invalid_credentials"
+            }, 401
+        color_data = request.get_json()
+        if not color_data['color_value']:
+            return{
+                'description': "The color needs a color hex value",
+                'error': 'missing_info'
+            }, 400
+        color_attrs: ColorInterface = dict(color_value = color_data['color_value'])
+        color: ColorModel = ColorService.create(color_attrs, self.app, db)
+        return{
+            'message': "Color created successfully",
+            'color': ColorService.json(color)
+        }, 201
     
     @jwt_required()
     def put(self):
-        raise NotImplementedError
+        claims = get_jwt()
+        if not claims['is_admin']:
+            return{
+                'description': "You need admin privileges",
+                'error': "invalid_credentials"
+            }, 401
+        color_data = request.get_json()
+        if not color_data['color_id']:
+            return{
+                'description': "The color id must be given",
+                'error': 'missing_info'
+            }, 400
+
+        if not color_data['color_value']:
+            return{
+                'description': "The color needs a new color hex value",
+                'error': 'missing_info'
+            }, 400
+        color: ColorModel = ColorService.retrieve_by_color_id(color_data['color_id'], self.app)
+        updates: ColorInterface = dict(color_value = color_data['color_value'])
+        color = ColorService.update(color, updates, self.app, db)
+
+        return{
+            'message': "Color updated successfully.",
+            'color': ColorService.json(color)
+        }
+
 
     @jwt_required()
     def delete(self):
-        raise NotImplementedError
+        claims = get_jwt()
+        if not claims['is_admin']:
+            return{
+                'description': "You need admin privileges",
+                'error': "invalid_credentials"
+            }, 401
+        color_data = request.get_json()
+        if not color_data['color_id']:
+            return{
+                'description': "The color id must be given",
+                'error': 'missing_info'
+            }, 400
+        ColorService.delete(color_data['color_id'], self.app, db)
+        return{
+            'message': 'Color deleted successfully'
+        }, 200
 
 
 class ColorsListResource(Resource):
@@ -30,6 +103,7 @@ class ColorsListResource(Resource):
     def __init__(self, app: Flask):
         self.app = app
 
-    @jwt_required()
     def get(self):
-        raise NotImplementedError
+        return{
+            'colors': [ColorService.json(color) for color in ColorService.retrieve_all(self.app)]
+        }
