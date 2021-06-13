6f60b2c8b74fdd2e68bf195b9d7426a0e8cdf2ae
abdelazizrashed <abdelaziz.y.rashed@gmail.com>
2021-06-13 18:12:58 +0200

    Remove the if condition in run.py

--- run.py
+++ run.py
@@ -5,6 +5,5 @@ from app.shared.db_man.service import db
 
 app = create_app(os.getenv("FLASK_ENV") or "prod")
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+with app.app_context():
+    db.create_all()
