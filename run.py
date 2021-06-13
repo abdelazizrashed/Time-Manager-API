57bfce6e39eea534a92e47df1d764168fdffa3d5
abdelazizrashed <abdelaziz.y.rashed@gmail.com>
2021-06-13 18:06:54 +0200

    Just forgot to create the app. Nothing to see here

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ import os
 from app import create_app
 from app.shared.db_man.service import db
 
+app = create_app(os.getenv("FLASK_ENV") or "prod")
 
 if __name__ == "__main__":
-    db.create_all()
+    with app.app_context():
+        db.create_all()
