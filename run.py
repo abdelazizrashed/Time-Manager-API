5f9e4ec98cadd196418b810202954675d87610fd
abdelazizrashed <abdelaziz.y.rashed@gmail.com>
2021-06-13 17:57:39 +0200

    Minor changes to run.py

--- run.py
+++ run.py
@@ -3,5 +3,6 @@ import os
 from app import create_app
 from app.shared.db_man.service import db
 
-db.create_all()
-app = create_app(os.getenv("FLASK_ENV") or "prod")
+
+if __name__ == "__main__":
+    db.create_all()
